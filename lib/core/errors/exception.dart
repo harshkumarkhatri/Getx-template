d9c8d112041cba7a06f633535a5b3f970d473468
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2024-01-09 16:10:55 +0530

    Updated custom exception file

--- lib/core/errors/exception.dart
+++ lib/core/errors/exception.dart
@@ -1,62 +1,62 @@
 import 'package:getx_initial_setup/core/values/strings.dart';
 
-abstract class ProdoException implements Exception {
+abstract class CustomException implements Exception {
   final String message;
-  ProdoException(this.message);
+  CustomException(this.message);
 
   @override
   String toString() => message;
 }
 
-class InternalErrorException extends ProdoException {
+class InternalErrorException extends CustomException {
   InternalErrorException([String? message])
       : super(message ?? internalErrorExceptionMessage);
 }
 
-class NoVehicleDataException extends ProdoException {
+class NoVehicleDataException extends CustomException {
   NoVehicleDataException([String? message])
       : super(message ?? internalErrorExceptionMessage);
 }
 
-class NetworkException extends ProdoException {
+class NetworkException extends CustomException {
   NetworkException([String? message])
       : super(message ?? networkExceptionMessage);
 }
 
-class BadRequestException extends ProdoException {
+class BadRequestException extends CustomException {
   BadRequestException([String? message])
       : super(message ?? badRequestExceptionMessage);
 }
 
-class UnauthorisedException extends ProdoException {
+class UnauthorisedException extends CustomException {
   UnauthorisedException([String? message])
       : super(message ?? unauthorisedExceptionMessage);
 }
 
-class NotFoundException extends ProdoException {
+class NotFoundException extends CustomException {
   NotFoundException([String? message])
       : super(message ?? notFoundExceptionMessage);
 }
 
-class NullDataException extends ProdoException {
+class NullDataException extends CustomException {
   NullDataException([String? message])
       : super(message ?? notFoundExceptionMessage);
 }
 
-class UploadDocsException extends ProdoException {
+class UploadDocsException extends CustomException {
   UploadDocsException([String? message])
       : super(message ?? notFoundExceptionMessage);
 }
 
-class FutureDateException extends ProdoException {
+class FutureDateException extends CustomException {
   FutureDateException([String? message])
       : super(message ?? futureDateException);
 }
 
-class AppUpdateException extends ProdoException {
+class AppUpdateException extends CustomException {
   AppUpdateException({String? message}) : super(message ?? appUpdateException);
 }
 
-class SwitchFormatException extends ProdoException {
+class SwitchFormatException extends CustomException {
   SwitchFormatException([String? message]) : super(message ?? formatException);
 }
