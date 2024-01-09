1f5ed658680bef4aaa891eaf1b0870bf4265c97e
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2024-01-09 16:10:34 +0530

    Added strings for exception and key file for adding the keys as per need

---
+++ lib/core/values/strings.dart
@@ -0,0 +1,30 @@
+//Calendar
+const calendarSummary = "Calendar Summary";
+const premiumNotDeducted = "Premium not deducted";
+const premiumDeductedButRefunded = 'Premium deducted but has been refunded';
+const drivingLicenceNumberStr = "Driving License Number";
+const firstNameStr = 'First Name';
+const licenceExpiryDateStr = 'License Expiry Date';
+const continueStr = 'Continue';
+const organizationStr = 'Organization';
+const individualStr = 'Individual';
+const internalErrorExceptionMessage =
+    "Something went wrong please try again after some time.";
+
+const networkExceptionMessage =
+    "Hey! Looks like you are offline. Please check your internet connectivity.";
+
+const badRequestExceptionMessage = "Bad Request Exception Occured";
+
+const unauthorisedExceptionMessage =
+    "Oops! For security purposes, you will have to login again.";
+
+const notFoundExceptionMessage =
+    "Not Found Exception: Nothing here. Please check back in sometime";
+
+const formatException = "The received data was of incorrect format";
+const futureDateException = "The received date is of future";
+const appUpdateException = "The App could not be updated";
+
+const addBalanceStr = 'ADD BALANCE';
+const noBillingInfoStr = 'No billing information found for this month';
